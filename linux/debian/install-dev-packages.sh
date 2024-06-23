8d34e9db2d769b59f5df4e77166820839c60f226
Sven Roeseler <sven@svero.de>
2024-06-23 13:45:52 +0200

    Debian - splitted JDK from other dev packages

--- linux/debian/install-dev-packages.sh
+++ linux/debian/install-dev-packages.sh
@@ -5,4 +5,4 @@ apt install -y build-essential gcc-12-locales binutils-doc make-doc gcc-doc \
 	glibc-doc libstdc++-12-doc gdb gdb-doc gfortran gfortran-doc flex \
 	flex-doc bison bison-doc m4-doc autoconf autoconf-doc automake \
 	libtool libtool-doc glibc-doc-reference doc-base debian-keyring \
-	manpages-dev manpages-de-dev openjdk-17-{doc,jdk,source}
+	manpages-dev manpages-de-dev
---
+++ linux/debian/install-jdk.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+# Install JDK plus docs and sources
+apt install -y openjdk-17-{doc,jdk,source}

61a8558ef527bbc28103d1d57bcc25e5209fa92c
Sven Roeseler <sven@svero.dev>
2024-07-10 15:40:02 +0200

    Some adjustments for Debian

--- linux/debian/install-multimedia-packages.sh
+++ linux/debian/install-multimedia-packages.sh
@@ -2,5 +2,4 @@
 
 # Install multimedia packages
 apt install -y libavcodec-extra ffmpeg ffmpeg-doc mplayer mplayer-doc \
-	vlc vlc-plugin-pipewire moc moc-ffmpeg-plugin gettext-doc \
-	libdvdread8 libdvdnav4 libdvdnav-doc
+	vlc vlc-plugin-pipewire moc moc-ffmpeg-plugin gettext-doc

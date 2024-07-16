086ccb4388bd3dbbc6f5735ef541e89450725f6b
Sven Roeseler <sven@svero.dev>
2024-07-16 20:30:33 +0200

    Added mtools and dos2unix to Debian scripts

--- linux/debian/install-base-packages.sh
+++ linux/debian/install-base-packages.sh
@@ -5,7 +5,7 @@ apt install -y tmux git git-doc vim vim-doc vim-scripts exuberant-ctags mc \
 	rar unrar arj zip unzip wget curl ncftp w3m btop \
 	htop powertop keepassxc kpcli rsync imagemagick imagemagick-doc \
 	manpages-de plymouth-themes apt-file aspell aspell-doc aspell-de \
-	aspell-en
+	aspell-en mtools dos2unix
 
 # Update the apt-file database
 apt-file update

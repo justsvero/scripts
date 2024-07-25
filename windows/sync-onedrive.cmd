61621dd772634a6447c9405fc09103f86afb6da8
Sven Roeseler <git@svero.dev>
2024-07-25 07:42:12 +0200

    Added OneDrive sync script for Windows

---
+++ windows/sync-onedrive.cmd
@@ -0,0 +1,4 @@
+@ECHO OFF
+
+XCOPY /F /S /E /D /Y %USERPROFILE%\OneDrive\Documents\ %USERPROFILE%\Documents\
+XCOPY /F /S /E /D /Y %USERPROFILE%\OneDrive\Pictures\Wallpapers\ %USERPROFILE%\Pictures\Wallpapers\

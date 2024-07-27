585c6c78de03643b3973830e9a9e6bf56a75ad14
Sven Roeseler <git@svero.dev>
2024-07-27 22:15:30 +0200

    Added apt-file and hunspell to Linux Mint base script

--- linux/mint/mint-22/install-base-packages.sh
+++ linux/mint/mint-22/install-base-packages.sh
@@ -3,5 +3,10 @@
 apt install -y keepassxc kpcli tmux git git-doc vim \
 	vim-scripts vim-doc exuberant-ctags wget curl zip \
 	unzip rar unrar arj mc links lynx w3m ncftp sharutils \
-	sharutils-doc aspell aspell-de aspell-en pwgen \
-	dos2unix mtools bat neofetch htop
+	sharutils-doc aspell aspell-de aspell-en hunspell \
+	hunspell-de-at-frami hunspell-de-ch-frami \
+	hunspell-de-de-frami hunspell-en-au hunspell-en-ca \
+	hunspell-en-gb hunspell-en-us hunspell-en-za pwgen \
+	dos2unix mtools bat neofetch htop apt-file
+
+apt-file update

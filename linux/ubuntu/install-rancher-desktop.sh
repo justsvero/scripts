94acaa8e6bc79793c63f1b27c5037089c2be9b99
Sven Roeseler <sven@svero.dev>
2024-10-28 08:15:10 +0100

    Added a script to install SUSE Rancher Desktop

---
+++ linux/ubuntu/install-rancher-desktop.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+# Enable repository
+curl -s https://download.opensuse.org/repositories/isv:/Rancher:/stable/deb/Release.key | gpg --dearmor | dd status=none of=/usr/share/keyrings/isv-rancher-stable-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/isv-rancher-stable-archive-keyring.gpg] https://download.opensuse.org/repositories/isv:/Rancher:/stable/deb/ ./' | dd status=none of=/etc/apt/sources.list.d/isv-rancher-stable.list
+
+# Refresh package database
+apt update
+
+# Install SUSE Rancher Desktop
+apt install rancher-desktop

26f716905a88ad667aa9f3b7d7fbe503b02ee4b7
Sven Roeseler <sven@svero.dev>
2024-11-04 07:23:47 +0100

    Adjustments for Linux Mint 22

--- linux/mint/install-qemu-kvm.sh
+++ linux/mint/install-qemu-kvm.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
-apt install -y qemu qemu-kvm qemu-system qemu-utils virt-manager \
-	libvirt-clients libvirt-doc uml-utilities bridge-utils net-tools
+apt install qemu-system qemu-user qemu-utils virt-manager \
+    libvirt-clients libvirt-clients-qemu libvirt-doc \
+    uml-utilities bridge-utils net-tools
+

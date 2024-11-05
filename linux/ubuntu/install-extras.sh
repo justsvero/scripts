5448e0ebe66c3d3d393a10325bb22ae9300e356d
Sven Roeseler <sven@svero.dev>
2024-11-05 22:39:25 +0100

    Added some packages to Ubuntu

--- linux/ubuntu/install-extras.sh
+++ linux/ubuntu/install-extras.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/sh
 
-apt install -y ubuntu-restricted-extras \
-	ffmpeg ffmpeg-doc mplayer mplayer-doc vlc moc moc-ffmpeg-plugin v4l-utils libdvd-pkg \
-	vim vim-doc vim-scripts exuberant-ctags keepassxc curl w3m ncftp mc arj rar unrar tmux dict \
-	apt-file wl-clipboard xsel pwgen pass build-essential binutils-doc gcc-doc glibc-doc \
-	libstdc++-13-doc make-doc cmake cmake-doc ninja-build git git-doc
+apt install -y ubuntu-restricted-extras ffmpeg ffmpeg-doc mplayer mplayer-doc vlc \
+	moc moc-ffmpeg-plugin v4l-utils libdvd-pkg vim vim-doc vim-scripts \
+	exuberant-ctags keepassxc curl w3m ncftp mc arj rar unrar tmux dict apt-file \
+	wl-clipboard xsel pwgen pass build-essential binutils-doc gcc-doc glibc-doc \
+	libstdc++-13-doc make-doc cmake cmake-doc ninja-build git git-doc \
+	sharutils sharutils-doc dos2unix mtools manpages-dev manpages-de-dev \
+	htop lm-sensors
 
 apt-file update

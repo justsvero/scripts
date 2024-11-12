02c5b55ba35acc5bd4d1af03cd3be9a7cf7823d5
Sven Roeseler <sven@svero.dev>
2024-11-12 23:23:12 +0100

    Some adjustments for Debian (added inxi and neofetch)

---
+++ linux/debian/install-basics.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# Install base packages
+apt install -y tmux git git-doc vim vim-doc vim-scripts exuberant-ctags mc \
+	rar unrar arj zip unzip wget curl ncftp w3m inxi btop htop powertop \
+	keepassxc kpcli rsync imagemagick imagemagick-doc manpages-de \
+	plymouth-themes apt-file aspell aspell-doc aspell-de aspell-en \
+	mtools dos2unix pass pwgen neofetch
+
+# Update the apt-file database
+apt-file update

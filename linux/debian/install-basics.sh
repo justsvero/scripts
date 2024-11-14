78b2c09d8305ba301939b6a778cec1fcd2da884a
Sven Roeseler <sven@svero.dev>
2024-11-14 22:56:44 +0100

    Added some more packages to Debian scripts

--- linux/debian/install-basics.sh
+++ linux/debian/install-basics.sh
@@ -5,7 +5,7 @@ apt install -y tmux git git-doc vim vim-doc vim-scripts exuberant-ctags mc \
 	rar unrar arj zip unzip wget curl ncftp w3m inxi btop htop powertop \
 	keepassxc kpcli rsync imagemagick imagemagick-doc manpages-de \
 	plymouth-themes apt-file aspell aspell-doc aspell-de aspell-en \
-	mtools dos2unix pass pwgen neofetch
+	mtools dos2unix pass pwgen neofetch lfm bat chromium chromium-l10n
 
 # Update the apt-file database
 apt-file update

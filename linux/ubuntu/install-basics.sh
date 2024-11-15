2c95bfbfc72e24898de0fad36743b26e266cd323
Sven Roeseler <sven@svero.dev>
2024-11-15 11:30:37 +0100

    Added some tools for Ubuntu

--- linux/ubuntu/install-basics.sh
+++ linux/ubuntu/install-basics.sh
@@ -4,4 +4,4 @@
 apt install -y vim vim-doc vim-scripts exuberant-ctags keepassxc curl w3m ncftp \
 	mc arj rar unrar tmux dict apt-file wl-clipboard xsel pwgen pass git git-doc \
 	sharutils sharutils-doc dos2unix mtools manpages-dev manpages-de-dev \
-	htop lm-sensors
+	inxi htop btop lm-sensors lfm bat neofetch hyfetch

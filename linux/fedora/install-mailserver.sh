b1fdeb2a0a2d15c42cb8d38a1a722586496a5de5
Sven Roeseler <sven@svero.dev>
2024-11-22 08:01:41 +0100

    Added Cyrus SASL packages to mail server installation on Fedora

--- linux/fedora/install-mailserver.sh
+++ linux/fedora/install-mailserver.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # Mail server
-dnf install -y postfix procmail mailx fetchmail mutt alpine
+dnf install -y postfix procmail fetchmail mutt alpine cyrus-sasl cyrus-sasl-plain cyrus-sasl-md5

32220617ff1ae32c1618cb0b8c5c976859988612
Sven Röseler <sven@svero.dev>
2024-12-18 20:05:13 +0100

    Added some more package to the script for Linux Mint

--- linux/mint/lmde/install-essentials-lmde.sh
+++ linux/mint/lmde/install-essentials-lmde.sh
@@ -7,11 +7,12 @@ apt update && apt upgrade -y
 apt install -y build-essential exuberant-ctags vim-nox vim-scripts git gitk \
 	git-gui curl wget ncftp w3m links sharutils urlview apt-file rsync mc arj \
 	zip unzip htop powertop openjdk-17-jdk openjdk-17-doc openjdk-17-source \
-	tmux screen keepassxc kpcli neofetch binutils-doc make-doc gcc-doc \
+	tmux screen keepassxc neofetch binutils-doc make-doc gcc-doc \
 	glibc-doc vim-doc git-doc manpages manpages-dev manpages-de \
 	manpages-de-dev imagemagick imagemagick-doc qemu-system qemu-utils \
-	qemu-user neofetch lm-sensors gfortran gfortran-doc gnucobol strace moc \
-	moc-ffmpeg-plugin bat
+	qemu-user lm-sensors gfortran gfortran-doc gnucobol strace moc \
+	moc-ffmpeg-plugin bat libstdc++-12-doc bison bison-doc flex flex-doc \
+	libtool libtool-doc gdb-doc autoconf autoconf-doc automake btop
 
 # Clean-up
 apt clean

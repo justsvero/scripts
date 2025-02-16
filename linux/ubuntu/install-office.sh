860d90a0af9a58eccc2bd73a0f0f2596b6105a21
Sven Röseler <sven@svero.dev>
2025-02-16 20:11:26 +0100

    Fix for Ubuntu's LibreOffice install script

--- linux/ubuntu/install-office.sh
+++ linux/ubuntu/install-office.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/sh
 
-apt install -y libreoffice libreoffice-help-de libreoffice-help-en-gb \
-	libreoffice-help-en-us libreoffice-l10n-de libreoffice-l10-en-gb libreoffice-l10-en-za
+apt install -y libreoffice libreoffice-help-de libreoffice-help-en-gb libreoffice-help-en-us libreoffice-l10n-de libreoffice-l10n-en-gb libreoffice-l10n-en-za

14a84a4901e2fa01ecce8de46872bd7050b0cfdc
Sven Röseler <sven@svero.dev>
2025-04-28 10:18:50 +0200

    Removed uml-utilities from Trixie script because it is at least currently not available

--- linux/debian/trixie/install-virtualization.sh
+++ linux/debian/trixie/install-virtualization.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 # Trixie: uml-utilities does not exist in the repos
-apt install -y qemu-system qemu-utils qemu-user virt-manager libvirt-clients libvirt-doc uml-utilities bridge-utils net-tools libvirt-l10n
+apt install -y qemu-system qemu-utils qemu-user virt-manager libvirt-clients libvirt-doc bridge-utils net-tools libvirt-l10n

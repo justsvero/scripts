dcab15bf03c93fbf195cb90c46edbde4e3eaebd2
Sven Röseler <sven@svero.dev>
2025-05-29 19:26:08 +0200

    Added "tree" to Mint base script

--- linux/mint/install-base-packages.sh
+++ linux/mint/install-base-packages.sh
@@ -8,7 +8,7 @@ apt install -y keepassxc kpcli tmux git git-doc vim \
 		hunspell-de-de-frami hunspell-en-au hunspell-en-ca \
 		hunspell-en-gb hunspell-en-us hunspell-en-za pwgen \
 		dos2unix mtools bat neofetch htop btop apt-file \
-		pass whois lfm
+		pass whois lfm tree
 
 		echo '#################################################################'
 		echo 'Please run "apt-file update" as root to update the local database'

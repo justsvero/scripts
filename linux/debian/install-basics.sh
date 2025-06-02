0395857498cc2ba2de0463391475a5a577ec6a56
Sven Röseler <sven@svero.dev>
2025-06-02 16:28:37 +0200

    Added more packages to Debian scripts (base and devel)

--- linux/debian/install-basics.sh
+++ linux/debian/install-basics.sh
@@ -6,7 +6,7 @@ apt install -y tmux git git-doc vim vim-doc vim-scripts exuberant-ctags \
 	keepassxc kpcli rsync imagemagick imagemagick-doc manpages-de \
 	plymouth-themes apt-file aspell aspell-doc aspell-de aspell-en \
 	mtools dos2unix pass pwgen neofetch bat chromium chromium-l10n \
-	whois lfm mc
+	whois lfm mc gawk gawk-doc gperf texinfo
 
 echo '#################################################################'
 echo 'Please run "apt-file update" as root to update the local database'
--- linux/debian/install-dev-packages.sh
+++ linux/debian/install-dev-packages.sh
@@ -6,4 +6,5 @@ apt install -y build-essential gcc-12-locales binutils-doc make-doc gcc-doc \
 	flex flex-doc bison bison-doc m4-doc autoconf autoconf-doc automake \
 	libtool libtool-doc glibc-doc-reference doc-base debian-keyring \
 	manpages-dev manpages-de-dev strace pkgconf libssl-dev libssl-doc \
-	libncurses-dev ncurses-doc libcurl4-openssl-dev libcurl4-doc zlib1g-dev
+	libncurses-dev ncurses-doc libcurl4-openssl-dev libcurl4-doc zlib1g-dev \
+	libelf-dev libcap-dev

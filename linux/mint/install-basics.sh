39cf51f964be3219300b607d93f8f6e6c66620c3
Sven Röseler <sven@svero.dev>
2025-06-29 19:54:57 +0200

    Added jq to basics script for Linux Mint

--- linux/mint/install-basics.sh
+++ linux/mint/install-basics.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
 apt install -y keepassxc kpcli tmux git git-doc vim \
-		vim-scripts vim-doc exuberant-ctags wget curl zip \
-		unzip rar unrar arj mc links lynx w3m ncftp sharutils \
-		sharutils-doc aspell aspell-de aspell-en hunspell \
-		hunspell-de-at-frami hunspell-de-ch-frami \
-		hunspell-de-de-frami hunspell-en-au hunspell-en-ca \
-		hunspell-en-gb hunspell-en-us hunspell-en-za pwgen \
-		dos2unix mtools bat neofetch htop btop apt-file \
-		pass whois lfm tree
+	vim-scripts vim-doc exuberant-ctags wget curl zip \
+	unzip rar unrar arj mc links lynx w3m ncftp sharutils \
+	sharutils-doc aspell aspell-de aspell-en hunspell \
+	hunspell-de-at-frami hunspell-de-ch-frami \
+	hunspell-de-de-frami hunspell-en-au hunspell-en-ca \
+	hunspell-en-gb hunspell-en-us hunspell-en-za pwgen \
+	dos2unix mtools bat neofetch htop btop apt-file \
+	pass whois lfm tree jq
 
-		echo '#################################################################'
-		echo 'Please run "apt-file update" as root to update the local database'
-		echo '#################################################################'
+echo '#################################################################'
+echo 'Please run "apt-file update" as root to update the local database'
+echo '#################################################################'

73bacf63b09f66751b11b3e983a45306175231bb
Sven Röseler <sven@svero.dev>
2025-08-19 19:55:34 +0200

    Ubuntu: Added Java 17

--- linux/ubuntu/install-openjdk.sh
+++ linux/ubuntu/install-openjdk.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
-apt install -y openjdk-21-jdk openjdk-21-source openjdk-21-doc openjdk-21-demo
+apt install -y \
+	openjdk-17-jdk openjdk-17-source openjdk-17-doc openjdk-17-demo \
+	openjdk-21-jdk openjdk-21-source openjdk-21-doc openjdk-21-demo

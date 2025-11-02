96b51f67668e8206aeb11e81d0da0d6a5eadea04
Sven Röseler <sven@svero.dev>
2025-11-02 08:25:17 +0100

    Ubuntu: some small adjustments

--- linux/ubuntu/install-basics.sh
+++ linux/ubuntu/install-basics.sh
@@ -3,9 +3,9 @@
 # Install several tools and applications
 apt install -y vim vim-doc vim-scripts exuberant-ctags keepassxc curl w3m ncftp \
 	arj rar unrar tmux dict apt-file wl-clipboard pwgen pass git git-doc \
-	sharutils sharutils-doc dos2unix mtools manpages-de-dev \
-	inxi htop btop lm-sensors lfm mc bat neofetch hyfetch whois texinfo \
-	gperf gawk gawk-doc
+	sharutils sharutils-doc dos2unix mtools manpages-de \
+	inxi htop btop lm-sensors mc bat neofetch whois texinfo \
+	gperf gawk gawk-doc screen bzip2 bzip2-doc m4 m4-doc
 
 echo '#################################################################'
 echo 'Please run "app-file update" as root to update the local database'

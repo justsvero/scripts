4f89455a9453cd3154128788fc4685b5f2f59706
Sven Röseler <sven@svero.dev>
2025-11-02 10:02:45 +0100

    Ubuntu: added some libraries to the multimedia install script

--- linux/ubuntu/install-multimedia.sh
+++ linux/ubuntu/install-multimedia.sh
@@ -2,4 +2,5 @@
 
 # Install multimedia packages
 apt install -y ubuntu-restricted-extras ffmpeg ffmpeg-doc mplayer mplayer-doc \
-	vlc moc moc-ffmpeg-plugin v4l-utils rhythmbox rhythmbox-doc 
+	vlc moc moc-ffmpeg-plugin v4l-utils rhythmbox rhythmbox-doc libmfx-gen1.2 \
+	onevpl-tools libmfx-tools

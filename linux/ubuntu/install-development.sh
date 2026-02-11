1f3b3d71434c96bf2f2dd557dc9fd22bc5a320b8
Sven Röseler <sven@svero.dev>
2026-02-11 10:20:56 +0100

    Ubuntu: added some packages and renamed the folder for Questing Quokka

--- linux/ubuntu/install-development.sh
+++ linux/ubuntu/install-development.sh
@@ -4,4 +4,6 @@
 apt install -y build-essential binutils-doc gcc-doc glibc-doc libstdc++-13-doc \
 	make-doc cmake cmake-doc ninja-build manpages-dev manpages-de-dev \
 	bison bison-doc flex flex-doc autoconf autoconf-doc automake libtool libtool-doc \
-	clang clang-18-doc lldb pkgconf gdb gdb-doc gcc-13-locales
+	clang clang-18-doc lldb pkgconf gdb gdb-doc gcc-13-locales \
+	gettext gettext-doc llvm-18-doc gfortran gfortran-doc ncurses-doc glibc-doc \
+	glibc-doc-reference
--- linux/ubuntu/install-multimedia.sh
+++ linux/ubuntu/install-multimedia.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/sh
 
 # Install multimedia packages
-apt install -y ubuntu-restricted-extras ffmpeg ffmpeg-doc mplayer mplayer-doc \
-	vlc moc moc-ffmpeg-plugin v4l-utils rhythmbox rhythmbox-doc libmfx-gen1.2 \
-	onevpl-tools libmfx-tools
+apt install -y ubuntu-restricted-extras ffmpeg ffmpeg-doc vlc vlc-plugin-pipewire v4l-utils libmfx-gen1.2 onevpl-tools libmfx-tools

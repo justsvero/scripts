9b69c6fc55a62e1de8b47b22b6bc7caa9d3c1ac6
Sven Röseler <sven@svero.dev>
2026-02-24 13:37:41 +0100

    Windows: added a check for Books folder

--- windows/clean-streams.cmd
+++ windows/clean-streams.cmd
@@ -1,6 +1,11 @@
 @ECHO OFF
 
-REM streams64 -s -d -nobanner %USERPROFILE%\Books
+IF EXIST %USERPROFILE%\Books (
+  streams64 -s -d -nobanner %USERPROFILE%\Books
+) ELSE (
+  ECHO Das Verzeichnis %USERPROFILE%\Books existiert nicht
+)
+
 streams64 -s -d -nobanner %USERPROFILE%\Documents
 streams64 -s -d -nobanner %USERPROFILE%\Downloads
 streams64 -s -d -nobanner %USERPROFILE%\Music

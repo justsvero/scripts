4834764a4f609c540c7ebdd523f9d53129c4e34f
Sven Röseler <sven@svero.dev>
2026-06-12 15:06:17 +0200

    Ubuntu 26.04: added meson to development script

--- linux/ubuntu/Resolute/install-development.sh
+++ linux/ubuntu/Resolute/install-development.sh
@@ -2,6 +2,6 @@
 
 # Development tools
 apt install -y build-essential binutils-doc gcc-doc glibc-doc libstdc++-15-doc \
-	make-doc cmake cmake-doc ninja-build manpages-dev manpages-de-dev \
-	bison bison-doc flex flex-doc autoconf autoconf-doc automake libtool libtool-doc \
-	clang clang-20-doc lldb pkgconf gdb gdb-doc gcc-15-locales gfortran gfortran-doc
+    make-doc cmake cmake-doc ninja-build meson manpages-dev manpages-de-dev \
+    bison bison-doc flex flex-doc autoconf autoconf-doc automake libtool libtool-doc \
+    clang clang-20-doc lldb pkgconf gdb gdb-doc gcc-15-locales gfortran gfortran-doc 
